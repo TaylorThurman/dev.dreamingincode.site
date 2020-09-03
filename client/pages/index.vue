0371a68bd7f448bcc93d7032211fad820bcf0701
okH3reWeGo <taylor.john.thurman@outlook.com>
2020-09-03 12:51:18 -0500

    upload site

---
+++ client/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="overlay">
+            <SleepyDude class="sleepy-dude" />
+            <p>Coming Soon</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SleepyDude from '../components/SleepyDude'
+
+    export default {
+        components: {
+            SleepyDude
+        },
+        data() {
+            return {}
+        },
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+        background: #373737;
+    }
+
+    .container {
+        height: 80vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .overlay {
+        position: absolute;
+        color: white;
+    }
+
+    .sleepy-dude {
+        margin-left: 55px
+    }
+
+    p {
+        margin: 10px 0 0 0;
+        font-size: 36px;
+    }
+
+    @media screen and (max-width: 480px) {
+        .overlay {
+            width: 50%;
+        }
+    }
+</style>
